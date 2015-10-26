327f18d06b1d7e9b07397c61924fe83f1e7da149
Zackary Lowery <zlowery@xcjs.com>
2015-10-26 19:54:20 -0400

    Initial check-in. Includes main form and semi-functional notification icon.

---
+++ TrayWebView/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrayWebView.TrayIcon
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			var trayContext = new TrayApplicationContext();
+			Application.Run(trayContext);
+		}
+	}
+}
---
+++ TrayWebView/TrayApplicationContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using TrayWebView.ChromiumUI;
+
+namespace TrayWebView.TrayIcon
+{
+	class TrayApplicationContext : ApplicationContext
+	{
+		private IContainer components;		// a list of components to dispose when the context is disposed
+		private NotifyIcon notifyIcon;      // the icon that sits in the system tray
+		private MainWindow chromiumForm = null;
+
+		public TrayApplicationContext()
+		{
+			InitializeContext();
+		}
+
+		private void InitializeContext()
+		{
+			components = new System.ComponentModel.Container();
+			notifyIcon = new NotifyIcon(components)
+			{
+				ContextMenuStrip = new ContextMenuStrip(),
+				Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath),
+				Text = "TrayWebView",
+				Visible = true
+			};
+			notifyIcon.Click += notifyIcon_Click;
+		}
+
+		private void notifyIcon_Click(object sender, EventArgs e)
+		{
+			if (chromiumForm == null)
+			{
+				chromiumForm = new MainWindow();
+				ElementHost.EnableModelessKeyboardInterop(chromiumForm);
+				chromiumForm.Show();
+
+			}
+			else
+			{
+				chromiumForm.Activate();
+			}
+		}
+	}
+}
