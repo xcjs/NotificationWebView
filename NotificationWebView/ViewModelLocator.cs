d61b5399677e209743e008aa162fe8259cd15a5f
Zackary Lowery <zlowery@xcjs.com>
2015-12-06 15:55:54 -0500

    Added ViewModelLocator for MMVMLight.

---
+++ NotificationWebView/ViewModelLocator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace NotificationWebView
+{
+	public class ViewModelLocator
+	{
+		public ViewModelLocator()
+		{
+			ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+		}
+	}
+}
