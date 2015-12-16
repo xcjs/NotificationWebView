0d98c753e2f93ccaef6c504d5e8edaf46a1d2523
Zackary Lowery <zlowery@xcjs.com>
2015-12-16 00:48:06 -0500

    Corrected the product name in the assembly information.

--- NotificationWebView/Properties/AssemblyInfo.cs
+++ NotificationWebView/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NotificationWebView.ChromiumUI")]
+[assembly: AssemblyTitle("NotificationWebView")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NotificationWebView.ChromiumUI")]
+[assembly: AssemblyProduct("NotificationWebView")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
